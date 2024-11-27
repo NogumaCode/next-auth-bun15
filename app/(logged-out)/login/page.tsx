092d483d54b0eea240c275d1045c751c2eeead04
NogumaCode <111834765+NogumaCode@users.noreply.github.com>
2024-11-27 22:13:52 +0900

    two factor auth setting and auth system comp

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -16,14 +16,22 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 import { Input } from "@/components/ui/input";
 import { passwordSchema } from "@/validation/passwordSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { loginWithCredentials } from "./actions";
+import { loginWithCredentials, preLoginCheck } from "./actions";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useState } from "react";
+import { useToast } from "@/hooks/use-toast";
 
 const formSchema = z.object({
   email: z.string().email("有効なメールアドレスを入力してください"),
@@ -31,6 +39,11 @@ const formSchema = z.object({
 });
 
 export default function Login() {
+  
+  const {toast} = useToast();
+  const [step, setStep] = useState(1);
+  
+  const [otp, setOtp] = useState("");
   const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -41,89 +54,164 @@ export default function Login() {
   });
 
   const handleSubmit = async (data: z.infer<typeof formSchema>) => {
-    const response = await loginWithCredentials({
+    const preLoginCheckResponse = await preLoginCheck({
       email: data.email,
       password: data.password,
     });
 
-    if (response?.error) {
+    if (preLoginCheckResponse.error) {
       form.setError("root", {
-        message: response.message,
+        message: preLoginCheckResponse.message,
       });
+      return;
+    }
+
+    if (preLoginCheckResponse.twoFactorActivated) {
+      setStep(2);
     } else {
-      router.push("/my-account");
+      const response = await loginWithCredentials({
+        email: data.email,
+        password: data.password,
+      });
+
+      if (response?.error) {
+        form.setError("root", {
+          message: response.message,
+        });
+      } else {
+        router.push("/my-account");
+      }
     }
   };
 
   const email = form.getValues("email");
 
+  const handleOTPSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const response = await loginWithCredentials({
+      email: form.getValues("email"),
+      password: form.getValues("password"),
+      token: otp,
+    });
+
+    if (response?.error) {
+      toast({
+        title:"コードが異なっております",
+        variant:"destructive"
+      })
+    } else {
+      router.push("/my-account");
+    }
+  };
+
   return (
     <main className="flex justify-center items-center min-h-screen">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>ログイン</CardTitle>
-          <CardDescription>アカウントにログインしてください</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(handleSubmit)}>
-              <fieldset
-                disabled={form.formState.isSubmitting}
-                className="flex flex-col gap-2"
+      {step === 1 && (
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>ログイン</CardTitle>
+            <CardDescription>アカウントにログインしてください</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)}>
+                <fieldset
+                  disabled={form.formState.isSubmitting}
+                  className="flex flex-col gap-2"
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>メールアドレス</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="email" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>パスワード</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="password" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {!!form.formState.errors.root?.message && (
+                    <FormMessage>
+                      {form.formState.errors.root.message}
+                    </FormMessage>
+                  )}
+                  <Button type="submit" className="mt-2">
+                    ログイン
+                  </Button>
+                </fieldset>
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <div className="text-muted-foreground text-xs">
+              アカウントを持っていないですか？{" "}
+              <Link href="/register" className="underline">
+                新規登録
+              </Link>
+            </div>
+            <div className="text-muted-foreground text-xs">
+              パスワードを忘れましたか？{" "}
+              <Link
+                href={`/password-reset${
+                  email ? `?email=${encodeURIComponent(email)}` : ""
+                }`}
+                className="underline"
               >
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>メールアドレス</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="email" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>パスワード</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="password" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {!!form.formState.errors.root?.message && (
-                  <FormMessage>
-                    {form.formState.errors.root.message}
-                  </FormMessage>
-                )}
-                <Button type="submit" className="mt-2">
-                  ログイン
-                </Button>
-              </fieldset>
+                パスワードリセット
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      )}
+      {step === 2 && (
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>ワンタイムパスコード</CardTitle>
+            <CardDescription>
+              Google Authenticator アプリに表示された WebDevEducation
+              のワンタイムパスコードを入力します。
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleOTPSubmit} className="flex flex-col gap-2">
+              <InputOTP maxLength={6} value={otp} onChange={setOtp}>
+                <InputOTPGroup>
+                  <InputOTPSlot index={0} />
+                  <InputOTPSlot index={1} />
+                  <InputOTPSlot index={2} />
+                </InputOTPGroup>
+                <InputOTPSeparator />
+                <InputOTPGroup>
+                  <InputOTPSlot index={3} />
+                  <InputOTPSlot index={4} />
+                  <InputOTPSlot index={5} />
+                </InputOTPGroup>
+              </InputOTP>
+              <Button disabled={otp.length !== 6} type="submit">
+                送信する
+              </Button>
+              <Button onClick={() => setStep(2)} variant="outline">
+                キャンセル
+              </Button>
             </form>
-          </Form>
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <div className="text-muted-foreground text-xs">
-            アカウントを持っていないですか？{" "}
-            <Link href="/register" className="underline">
-              新規登録
-            </Link>
-          </div>
-          <div className="text-muted-foreground text-xs">
-            パスワードを忘れましたか？{" "}
-            <Link href={`/password-reset${email ? `?email=${encodeURIComponent(email)}`:''}`} className="underline">
-              パスワードリセット
-            </Link>
-          </div>
-        </CardFooter>
-      </Card>
+          </CardContent>
+        </Card>
+      )}
     </main>
   );
 }
--- auth.ts
+++ auth.ts
@@ -1,45 +1,65 @@
 import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials"
+import Credentials from "next-auth/providers/credentials";
 import { users } from "./db/usersSchema";
 import db from "./db/drizzle";
 import { eq } from "drizzle-orm";
 import { compare } from "bcryptjs";
+import { authenticator } from "otplib";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  callbacks:{
-    jwt({token,user}){
-      if(user){
-        token.id = user.id
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
       }
       return token;
     },
-    session({session,token}){
+    session({ session, token }) {
       session.user.id = token.id as string;
       return session;
-    }
+    },
   },
   providers: [
     Credentials({
       credentials: {
         email: {},
         password: {},
+        token:{}
       },
-      async authorize(credentials){
-        const [user] = await db.select().from(users).where(eq(users.email,credentials.email as string))
+      async authorize(credentials) {
+        const [user] = await db
+          .select()
+          .from(users)
+          .where(eq(users.email, credentials.email as string));
 
-        if(!user){
-          throw new Error("認証できませんでした。")
-        }else{
-          const passwordCorrect = await compare(credentials.password as string ,user.password!)
-            if(!passwordCorrect){
-              throw new Error("認証できませんでした。")
-            }
+        if (!user) {
+          throw new Error("認証できませんでした。");
+        } else {
+          const passwordCorrect = await compare(
+            credentials.password as string,
+            user.password!
+          );
+          if (!passwordCorrect) {
+            throw new Error("認証できませんでした。");
+          }
+
+          if(user.twoFactorActivated){
+            const tokenValid = authenticator.check(
+              credentials.token as string,
+              user.twoFactorSecret ?? ""
+            )
+
+            if (!tokenValid) {
+              throw new Error("認証できませんでした。");
+            }            
+
+          }
         }
-        return{
-          id:user.id.toString(),
-          email:user.email,
-        }
-      }
+        return {
+          id: user.id.toString(),
+          email: user.email,
+        };
+      },
     }),
   ],
 });
